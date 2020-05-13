1473bbc986dddc2e2d026aec4610e127c4043a13
Dmitry Dutin <grozzlou@gmail.com>
2020-05-13 18:17:40 +0300

    create app

---
+++ README.md
@@ -0,0 +1,4 @@
+# coursework
+Made by Dmitry Dutin
+
+You can see it in action at: https://courseworkit.herokuapp.com/
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className='App'>
+            Server working
+        </div>
+    )
+}
+
+export default App
