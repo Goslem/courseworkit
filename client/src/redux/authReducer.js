1699a87ceabe104c0f81b79b29b39608aa0ea18c
Dmitry Dutin <grozzlou@gmail.com>
2020-05-19 19:45:40 +0300

    fix bugs

--- client/src/redux/authReducer.js
+++ client/src/redux/authReducer.js
@@ -75,7 +75,5 @@ export const getAuthUserData = () => (dispatch) => {
             let { id, login } = response.data
             dispatch(setUserData(id, login, true))
         }
-
-        console.log(response)
     })
 }
